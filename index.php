749e9e4a7030c582119bf5632ac82e2460e3a59c
Olobodomar <valdecidesouzaneto@gmail.com>
2022-08-24 09:36:27 -0300

    resolvi o conflito de testes

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 ?<?php 
 
 <<<<<<< HEAD
-echo "teste 123415215";
-
-echo "xampson  ";
-
-=======
 echo "teste";
 
-echo "teste2"; 
->>>>>>> f24d0e255d11a62e02ea38a24393db3a10355c39
+echo "teste2";
+
 
  ?>
